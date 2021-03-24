31fc024a25d779ce95921a5e6202a0334d0676ed
Mengmei Tu <redmmeii@gmail.com>
2021-03-23 22:42:42 -0500

    build

---
+++ client/build/precache-manifest.ff412bb7b87f03f654b2875bbade59f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63aeb3ab43c57efe2c922efaf7c75fee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "465456b2e23a3b621937",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "1f32520d8609cb8e3845",
+    "url": "/static/css/main.4c27bbfc.chunk.css"
+  },
+  {
+    "revision": "465456b2e23a3b621937",
+    "url": "/static/js/2.9a9bdda8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.9a9bdda8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f32520d8609cb8e3845",
+    "url": "/static/js/main.c58be36e.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
